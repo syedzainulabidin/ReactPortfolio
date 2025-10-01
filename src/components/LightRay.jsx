84c3cc8b715d668e2265add0c56e98be71500223
Zain <syedzainulabidinali@gmail.com>
2025-10-01 13:44:32 +0500

    Color Correction

--- src/components/LightRay.jsx
+++ src/components/LightRay.jsx
@@ -6,13 +6,13 @@ const LightRay = () => {
     <div style={{ width: "100dvw", height: "100dvh", position: "relative" }}>
       <LightRays
         raysOrigin="top-center"
-        raysColor="white"
+        raysColor="#e5a9ff"
         raysSpeed={1.5}
         lightSpread={0.8}
         rayLength={1.2}
         followMouse={true}
         mouseInfluence={0.1}
-        noiseAmount={0.1}
+        noiseAmount={0.05}
         distortion={0.05}
         className="custom-rays"
       />
